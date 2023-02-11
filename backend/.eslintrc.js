69df091123a56b9c525157d1d138d9818b210b3c
Kacper Bylicki <60986172+kacperbylicki@users.noreply.github.com>
2023-02-11 20:16:08 +0100

    chore: adjust eslint and prettier configuration to discussed standard

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,30 +1,52 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  root: true,
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:security/recommended',
     'plugin:prettier/recommended',
+    'prettier',
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
+  plugins: [
+    'prettier',
+    '@typescript-eslint/eslint-plugin',
+    'sort-imports-es6-autofix',
+  ],
   ignorePatterns: [
     '.eslintrc.js', 'node_modules', 'dist', 'data'
    ],
   rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'warn',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
+    'import/prefer-default-export': 0,
+    'no-throw-literal': 0,
+    'prefer-const': 0,
+    'no-console': 'warn',
+    'no-prototype-builtins': 0,
     'prefer-rest-params': 'off',
-    "@typescript-eslint/explicit-function-return-type": "warn",
-    "no-console": "warn",
+    'security/detect-object-injection': 0,
+    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_', args: 'none' }],
+    'sort-imports-es6-autofix/sort-imports-es6': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'single', 'all', 'multiple'],
+      },
+    ],
   },
 };
