78c95153268f40dceb53a1c79fc9b81274bcff4c
Jakub <jakub.barczewski@netguru.com>
2023-02-11 19:04:30 +0100

    Barebones backend setup

---
+++ backend/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:security/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: [
+    '.eslintrc.js', 'node_modules', 'dist', 'data'
+   ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'prefer-rest-params': 'off',
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    "no-console": "warn",
+  },
+};
