e763b9b9ad60e08247fa86c52197ca079c2f3927
Kacper Bylicki <kacper@bylicki.dev>
2023-02-19 23:11:13 +0100

    fix(terraform): change remote backend tfstate file names for different apps

--- infrastructure/environments/production/application/backend.tf
+++ infrastructure/environments/production/application/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "trashify-tstate-rg"
     storage_account_name = "trashifytfstate31919"
     container_name       = "tfstate"
-    key                  = "actions.tfstate"
+    key                  = "application.tfstate"
   }
 }
