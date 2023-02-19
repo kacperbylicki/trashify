46b8585227900223f7c02bc78d81ec9b3cb28b27
Kacper Bylicki <kacper@bylicki.dev>
2023-02-19 18:35:06 +0100

    refactor(infrastructure): remove unnecessary staging env terraform configs and ci/cd workflows

--- infrastructure/environments/staging/machine-learning/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "trashify-tfstate-rg"
-    storage_account_name = "trashifytfstate30407"
-    container_name       = "tfstate"
-    key                  = "actions.tfstate"
-  }
-}
--- infrastructure/environments/staging/machine-learning/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "azure_cognitive_custom_vision_id" {
-  value = module.cognitive-services-custom-vision.id
-}
