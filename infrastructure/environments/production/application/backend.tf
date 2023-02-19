ba3c1f114dab1bbcdd977cb0dd7515e2a077eeb3
Kacper Bylicki <kacper@bylicki.dev>
2023-02-19 18:30:26 +0100

    chore(terraform): adjust storage account name for tfstate backend

--- infrastructure/environments/production/application/backend.tf
+++ infrastructure/environments/production/application/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "trashify-tfstate-rg"
-    storage_account_name = "trashifytfstate30407"
+    storage_account_name = "trashifytfstate31919"
     container_name       = "tfstate"
     key                  = "actions.tfstate"
   }
