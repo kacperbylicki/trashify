13e910b08f26a14aa45732823db2009427900253
Kacper Bylicki <kacper@bylicki.dev>
2023-02-23 17:56:37 +0100

    feat(terraform): add blob storage module and refactor infrastructure setup

--- infrastructure/modules/storage-blob/variables.tf
+++ infrastructure/modules/storage-blob/variables.tf
@@ -1,13 +1,11 @@
 variable "resource_group" {
   type        = string
   description = "The resource group"
-  default     = ""
 }
 
 variable "application_name" {
   type        = string
   description = "The name of your application"
-  default     = ""
 }
 
 variable "environment" {
@@ -18,5 +16,4 @@ variable "environment" {
 variable "location" {
   type        = string
   description = "The Azure region where all resources in this example should be created"
-  default     = ""
 }
