8423d77e7de0f1161418a205e63099f48140301c
Kacper Bylicki <60986172+kacperbylicki@users.noreply.github.com>
2023-09-23 20:18:27 +0200

    feat: add more env variables to deployment scripts (#54)
    * feat: add more env variables to deployment scripts
    
    * feat: comment module dependant on commented variables

--- apps/api-gateway/src/config/app.config.ts
+++ apps/api-gateway/src/config/app.config.ts
@@ -14,21 +14,22 @@ export class AppConfig {
   @IsInt()
   port!: number;
 
-  @From('API_GATEWAY_REDIS_CACHE_HOST')
-  @IsString()
-  apiGatewayCacheHost!: string;
+  // TODO: Uncomment this when redis service is running
+  // @From('API_GATEWAY_REDIS_CACHE_HOST')
+  // @IsString()
+  // apiGatewayCacheHost!: string;
 
-  @From('API_GATEWAY_REDIS_CACHE_PORT')
-  @IsInt()
-  apiGatewayCachePort!: number;
+  // @From('API_GATEWAY_REDIS_CACHE_PORT')
+  // @IsInt()
+  // apiGatewayCachePort!: number;
 
-  @From('API_GATEWAY_REDIS_CACHE_PASSWORD')
-  @IsString()
-  apiGatewayCachePassword!: string;
+  // @From('API_GATEWAY_REDIS_CACHE_PASSWORD')
+  // @IsString()
+  // apiGatewayCachePassword!: string;
 
-  @From('API_GATEWAY_REDIS_CACHE_TTL')
-  @IsInt()
-  apiGatewayCacheTTL!: number;
+  // @From('API_GATEWAY_REDIS_CACHE_TTL')
+  // @IsInt()
+  // apiGatewayCacheTTL!: number;
 
   @From('ACCOUNTS_SERVICE_URL')
   @IsString()
